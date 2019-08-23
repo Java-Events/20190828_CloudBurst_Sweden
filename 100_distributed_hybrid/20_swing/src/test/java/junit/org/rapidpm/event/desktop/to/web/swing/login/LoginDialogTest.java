59b30cd4acd1cdeb4c76e56b09de2f16afea9775
Sven Ruppert <sven.ruppert@gmail.com>
2019-08-23 22:36:27 +0200

    disabled Swing tests for Docker Builds

--- 100_distributed_hybrid/20_swing/src/test/java/junit/org/rapidpm/event/desktop/to/web/swing/login/LoginDialogTest.java
+++ 100_distributed_hybrid/20_swing/src/test/java/junit/org/rapidpm/event/desktop/to/web/swing/login/LoginDialogTest.java
@@ -1,15 +1,11 @@
 package junit.org.rapidpm.event.desktop.to.web.swing.login;
 
-import org.assertj.swing.core.matcher.DialogMatcher;
-import org.assertj.swing.core.matcher.JButtonMatcher;
-import org.assertj.swing.fixture.DialogFixture;
 import org.junit.jupiter.api.*;
-import org.rapidpm.event.desktop.to.web.swing.login.LoginDialog;
 
 //@ExtendWith(ServerExtension.class)
 public class LoginDialogTest {
 
-  private DialogFixture fixture;
+//  private DialogFixture fixture;
 
   @BeforeAll
   static void beforeAll() {
@@ -31,29 +27,29 @@ public class LoginDialogTest {
   @Test
   @Disabled
   void testLogin001() {
-    fixture.textBox(LoginDialog.ID_USERNAME)
-           .setText("admin");
-    fixture.textBox(LoginDialog.ID_PASSWORD)
-           .setText("admin");
-    fixture.button(LoginDialog.ID_BTN_LOGIN)
-           .click();
-
-    fixture.dialog(DialogMatcher.withTitle("Login OK"))
-           .button(JButtonMatcher.withText("OK"))
-           .click();
+//    fixture.textBox(LoginDialog.ID_USERNAME)
+//           .setText("admin");
+//    fixture.textBox(LoginDialog.ID_PASSWORD)
+//           .setText("admin");
+//    fixture.button(LoginDialog.ID_BTN_LOGIN)
+//           .click();
+//
+//    fixture.dialog(DialogMatcher.withTitle("Login OK"))
+//           .button(JButtonMatcher.withText("OK"))
+//           .click();
   }
 
   @Test
   @Disabled
   void testLogin002() {
-    fixture.textBox(LoginDialog.ID_USERNAME)
-           .setText("admin");
-    fixture.textBox(LoginDialog.ID_PASSWORD)
-           .setText("xx");
-    fixture.button(LoginDialog.ID_BTN_LOGIN)
-           .click();
-    fixture.dialog(DialogMatcher.withTitle("Login not allowed"))
-           .button(JButtonMatcher.withText("OK"))
-           .click();
+//    fixture.textBox(LoginDialog.ID_USERNAME)
+//           .setText("admin");
+//    fixture.textBox(LoginDialog.ID_PASSWORD)
+//           .setText("xx");
+//    fixture.button(LoginDialog.ID_BTN_LOGIN)
+//           .click();
+//    fixture.dialog(DialogMatcher.withTitle("Login not allowed"))
+//           .button(JButtonMatcher.withText("OK"))
+//           .click();
   }
 }
